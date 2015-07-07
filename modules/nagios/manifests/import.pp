f218225b9ea28ec3b14054dc053a0304288ba310
eholzbach <ericholzbach@gmail.com>
2015-07-07 16:41:41 -0700

    enable blacklist for other nagios components

--- modules/nagios/manifests/import.pp
+++ modules/nagios/manifests/import.pp
@@ -1,26 +1,31 @@
 class nagios::import {
 
-  Nagios_command <<||>> {
-    notify  => Class['nagios::service'],
-    require => Class['nagios::install'],
-    target  => '/usr/local/etc/nagios/nagios_command.cfg',
-  }
+  $monitor_host = str2bool(hiera('nagios::client::monitor_host'))
 
-  Nagios_host <<||>> {
-    notify  => Class['nagios::service'],
-    require => Class['nagios::install'],
-    target  => '/usr/local/etc/nagios/nagios_host.cfg',
-  }
+  if $monitor_host == true {
 
-  Nagios_hostgroup <<||>> {
-    notify  => Class['nagios::service'],
-    require => Class['nagios::install'],
-    target  => '/usr/local/etc/nagios/nagios_hostgroup.cfg',
-  }
+    Nagios_command <<||>> {
+      notify  => Class['nagios::service'],
+      require => Class['nagios::install'],
+      target  => '/usr/local/etc/nagios/nagios_command.cfg',
+    }
 
-  Nagios_service <<||>> {
-    notify  => Class['nagios::service'],
-    require => Class['nagios::install'],
-    target  => '/usr/local/etc/nagios/nagios_service.cfg',
+    Nagios_host <<||>> {
+      notify  => Class['nagios::service'],
+      require => Class['nagios::install'],
+      target  => '/usr/local/etc/nagios/nagios_host.cfg',
+    }
+
+    Nagios_hostgroup <<||>> {
+      notify  => Class['nagios::service'],
+      require => Class['nagios::install'],
+      target  => '/usr/local/etc/nagios/nagios_hostgroup.cfg',
+    }
+
+    Nagios_service <<||>> {
+      notify  => Class['nagios::service'],
+      require => Class['nagios::install'],
+      target  => '/usr/local/etc/nagios/nagios_service.cfg',
+    }
   }
 }
