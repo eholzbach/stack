5d5fef7db2899f1734eea2dec643c3285e03a281
eholzbach <ericholzbach@gmail.com>
2015-07-07 17:09:43 -0700

    screw nagios

--- modules/nagios/manifests/client.pp
+++ modules/nagios/manifests/client.pp
@@ -1,18 +1,14 @@
 class nagios::client {
 
-  $monitor_host = str2bool(hiera('nagios::client::monitor_host'))
-
-  if $monitor_host == true {
-    @@nagios_host {"${::hostname}":
-      ensure                => present,
-      address               => $::ipaddress,
-      group                 => wheel,
-      hostgroups            => "${::virtual}, ${::kernel}",
-      mode                  => '0644',
-      notifications_enabled => $notifications_enabled,
-      owner                 => root,
-      use                   => 'generic-server',
-    }
+  @@nagios_host {"${::hostname}":
+    ensure                => present,
+    address               => $::ipaddress,
+    group                 => wheel,
+    hostgroups            => "${::virtual}, ${::kernel}",
+    mode                  => '0644',
+    notifications_enabled => $notifications_enabled,
+    owner                 => root,
+    use                   => 'generic-server',
   }
 }
 
