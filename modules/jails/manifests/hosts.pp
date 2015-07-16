b74937930411979380df4e10e4afaaa0e2bfa58f
eholzbach <ericholzbach@gmail.com>
2015-07-16 15:14:43 -0700

    jails module config with vnet ifaces

--- modules/jails/manifests/hosts.pp
+++ modules/jails/manifests/hosts.pp
@@ -1,8 +1,11 @@
 define jails::hosts (
   $allow_raw_sockets = undef,
+  $gate              = undef,
   $host_hostname     = undef,
   $interface         = undef,
+  $inumber           = undef,
   $ip4_addr          = undef,
+  $mask              = undef,
 ) {
 
   file { '/etc/jail.conf':
--- modules/jails/manifests/init.pp
+++ modules/jails/manifests/init.pp
@@ -4,6 +4,13 @@ class jails (
   $jailhost = hiera('jails::hosts')
   create_resources('jails::hosts', $jailhost)
 
+  augeas { 'jails_loader_conf':
+    context => '/files/boot/loader.conf',
+    changes => [
+      "set if_epair_load '\"YES\"'",
+    ],
+  }
+
   augeas { 'jails_rc_conf':
     context => '/files/etc/rc.conf',
     changes => [
